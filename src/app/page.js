30df458a4808b620de5372d01e538e1ccdbd90c4
Giovanna de Souza Bregantin <gbregantin-ieg@instituto.ad>
2024-05-06 09:21:50 -0300

    stage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import { searchbar } from "../components/searchBar";
+import { navbar } from "../components/navbar";
 
 export default function Home() {
   return (
     <div className="bg-primary h-screen">
-      {searchbar()}
+      {navbar()}
       <div></div>
     </div>
   );
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+export function navbar() {
+    return (
+      <div className="w-96 h-full bg-secondary">
+
+      </div>
+    );
+  }
