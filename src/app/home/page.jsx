5f57a77cb31228527484589028c5dcc699416364
Giovanna de Souza Bregantin <gbregantin-ieg@instituto.ad>
2024-08-02 07:45:01 -0300

    feat: nome do usuário

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Partials/Navbar/Navbar";
 import { VariationDiv } from "@/components/UI/variation/variationDiv";
 import { walletValue } from "@/components/Partials/walletValue";
+import { welcomeName } from "@/components/Partials/welcomeName";
 import { searchbar } from "@/components/Partials/searchBar";
 
 export default function Home() {
@@ -12,7 +13,10 @@ export default function Home() {
       <div className="bg-primary w-full">
         {searchbar()}
         <VariationDiv />
-        {walletValue()}
+        <div className="flex justify-between px-4">
+          <div>{welcomeName()}</div>
+          <div>{walletValue()}</div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Partials/welcomeName.jsx
@@ -0,0 +1,9 @@
+export function welcomeName() {
+    return (
+      <div className="text-left pr-3 sm:pr-8 ml-6 mt-6 sm:ml-8 sm:mt-10 lg:ml-9 lg:mt-12">
+        <p className="text-gray text-xs sm:text-sm lg:text-lg">Bem vindo,</p>
+        <h1 className="text-white text-xl sm:text-2xl lg:text-3xl font-semibold pt-0.5 sm:pt-1">Ismael Amex</h1>
+      </div>
+    );
+  }
+  
