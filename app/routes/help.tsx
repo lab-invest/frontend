e5d2650c1f766bcdd520d3398edac738b0736d86
ismael <ismaelcosta949@IEGNTBALU0814.instituto.ad>
2024-08-30 07:26:31 -0300

    feat: adding new routes

---
+++ app/routes/help.tsx
@@ -0,0 +1,21 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { MetaFunction } from "@remix-run/react";
+import { Navbar } from "~/components";
+import { ensureAuthenticated } from "~/utils/session.server";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Ajuda" }];
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return await ensureAuthenticated(request);
+};
+
+export default function Help() {
+  return (
+    <div className="flex h-screen">
+      <Navbar />
+      <div className="flex flex-col bg-primary w-full px-10 gap-4 pt-6"></div>
+    </div>
+  );
+}
