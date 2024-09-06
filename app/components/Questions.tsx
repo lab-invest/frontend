059145c06b57d291b82b5bc52debf5d9e6e790a5
Giovanna de Souza Bregantin <gbregantin-ieg@instituto.ad>
2024-09-06 08:27:37 -0300

    ismael

--- app/components/Questions.tsx
+++ app/components/Questions.tsx
@@ -5,7 +5,7 @@ interface QuestionProps {
   
   export default function Question({ question, answer }: QuestionProps) {
     return (
-      <div className="bg-gray-900 mt-0 text-white p-4 sm:p-6 md:p-8 lg:p-6 rounded-md max-w-full">
+      <div className="bg-gray-900 text-white p-4 sm:p-6 md:p-8 lg:p-6 rounded-md max-w-full">
         <h2 className=" font-semibold mb-2 sm:mb-4 sm:text-[1rem] lg:mb-4 lg:text-[1.375rem]">
           {question}
         </h2>
